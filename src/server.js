7be9482fbb629e340be5dc468e179bd8dd7c1401
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2023-12-25 17:22:50 +0700

    EJS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
+const { render } = require('ejs')
 const express = require('express')// common js
+const path = require('path')
 const app = express()// app express
 const port = 9000//port
 
@@ -9,8 +11,11 @@ app.get('/', (req, res) => {
 
 //khai báo route
 app.get('/1', (req, res) => {
-    res.send('Hello World! Express với Quang Thái với trang 1')
+    res.render('sample.ejs')
 })
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 app.get('/abc', (req, res) => {
     res.send('<h1>Khai báo thêm route</h1>')
