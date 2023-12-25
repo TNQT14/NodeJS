3f597ade14ac26689609ed2fbe6a155abffadb5e
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2023-12-25 16:48:51 +0700

    Khai báo thêm route

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/1', (req, res) => {
     res.send('Hello World! Express với Quang Thái với trang 1')
 })
 
+app.get('/abc', (req, res) => {
+    res.send('<h1>Khai báo thêm route</h1>')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
