ffb0356d7ca4e054184646b02f4e4a501bd9ee40
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2023-12-25 16:43:41 +0700

    Khai báo route

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 9000
+const express = require('express')// common js
+const app = express()// app express
+const port = 9000//port
 
+//khai báo route
 app.get('/', (req, res) => {
-    res.send('Hello World! Express với Qaung Thái')
+    res.send('Hello World! Express với Quang Thái')
+})
+
+//khai báo route
+app.get('/1', (req, res) => {
+    res.send('Hello World! Express với Quang Thái với trang 1')
 })
 
 app.listen(port, () => {
