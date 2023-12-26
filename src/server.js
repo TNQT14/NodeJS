d253eeefe292252d4ad4d6c8adb0ce8c0e84670c
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2023-12-26 14:49:45 +0700

    fix env

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const { render } = require('ejs')
 const express = require('express')// common js
+require('dotenv').config()
+console.log(process.env)
 const path = require('path')
 const app = express()// app express
-const port = process.env //port
+const port = process.env.PORT || 8888 //port
+const hostname = process.env.HOST_NAME
 
 //khai báo route
 app.get('/', (req, res) => {
@@ -16,11 +19,10 @@ app.get('/1', (req, res) => {
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
-
 app.get('/abc', (req, res) => {
     res.send('<h1>Khai báo thêm route</h1>')
 })
 
-app.listen(port, () => {
+app.listen(port, hostname => {
     console.log(`Example app listening on port ${port}`)
 })
